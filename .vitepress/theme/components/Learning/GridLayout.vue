be9928cd43c4bd40e46ceb4b1baa9b6edcd136f1
Yexu <xlin129421@gmail.com>
2025-05-07 18:04:31 +0800

    📖: 新增文章

---
+++ .vitepress/theme/components/Learning/GridLayout.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="border-1 border-gray-50">
+        <div class="grid grid-cols-9 gap-2">
+            <div class="grid-item bg-amber-100">amber-1</div>
+            <div class="grid-item bg-amber-200">amber-2</div>
+            <div class="grid-item bg-amber-300">amber-3</div>
+            <div class="grid-item bg-amber-400">amber-4</div>
+            <div class="grid-item bg-amber-500">amber-5</div>
+            <div class="grid-item bg-amber-600">amber-6</div>
+            <div class="grid-item bg-amber-700">amber-7</div>
+            <div class="grid-item bg-amber-800">amber-8</div>
+            <div class="grid-item bg-amber-900">amber-9</div>
+            <div class="grid-item bg-blue-100">blue-1</div>
+            <div class="grid-item bg-blue-200">blue-2</div>
+            <div class="grid-item bg-blue-300">blue-3</div>
+            <div class="grid-item bg-blue-400">blue-4</div>
+            <div class="grid-item bg-blue-500">blue-5</div>
+            <div class="grid-item bg-blue-600">blue-6</div>
+
+        </div>
+    </div>
+    
+</template>
+
+<style scoped>
+.grid-item{
+    @apply  w-[100px] h-[100px] flex justify-center items-center
+}
+</style>
